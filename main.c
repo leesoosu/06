5d3bb32cb5a5074d28e7c9d56dec088fc99c3eae
leesoosu <144989207+leesoosu@users.noreply.github.com>
2023-10-13 13:30:43 +0900

    5

--- main.c
+++ main.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a)
-{
-     return(a*a);
+int factorial(int x) {
+   int y=1, i=1;
+   if (x==1)
+      return 1;
+   else{
+      for(i=1;i<=x;i++)
+      y*=i;
+   }
+   return y;
 }
 
-int main()
-{
-    int a = 2;
-    a= square(a);
-    printf("a=%i\n" ,a);
-  
-    system("PAUSE");
-    return 0;
+int sub(int a,int b){
+   if(a>b)
+      return a-b;
+   else
+      return b-a;
+}
+int combination(int n,int r){
+   return factorial(n)/(factorial(sub(n,r))*factorial(r));
+}
+int main(){
+   
+   int n,r;
+   printf("input n: "); 
+   scanf("%d",&n);
+   printf("input r: "); 
+   scanf("%d",&r);
+   printf("n combination r %d\n",combination(n,r));
+   system("PAUSE");   
+   return 0;
 }
